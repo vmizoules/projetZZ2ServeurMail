ed4da37cb5822407c2f9872dc9ea332d45e4294e
vmizoules <dark-vince15@hotmail.fr>
2015-12-08 18:29:50 +0100

    Forward works!

--- docker/postfix_server/config.sh
+++ docker/postfix_server/config.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-echo "Configuring Postfix..."
-
-echo "Update mailname"
-echo "localhost" > /etc/mailname
-
-echo "Add users..."
-useradd alexandre -g mail
-useradd vincent -g mail
-
-echo "Update aliases..."
-newaliases
-
 exit 0
--- docker/postfix_server/run.sh
+++ docker/postfix_server/run.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
+# Configuration
+
+echo "Configuring Postfix..."
+
+chown -R postfix /etc/postfix
+chgrp -R postfix /etc/postfix
+chmod -R ugo+rwx /etc/postfix
+
+echo "localhost" > /etc/mailname
+
+echo "Add users..."
+useradd alexandre -g mail
+useradd pierre -g mail
+useradd vincent -g mail
+
+echo "Update aliases..."
+newaliases
+
+echo "Read forward file..."
+postmap /etc/postfix/virtual
+
+# Launch services
+
 echo "Starting postfix..."
 service rsyslog start > /dev/null 2>&1
 service postfix start > /dev/null 2>&1
-sleep 2
+sleep 2 ; echo -e
 
-echo "Test mail..."
-sudo -u alexandre echo "Bonjour Vincent"|mail -s "Bjr" vincent@localhost
-echo '$alexandre: echo "Bonjour Vincent"|mail -s "Bjr" vincent@localhost'
+# Test
+
+echo "-- Test --"
+sudo -u alexandre echo "Bonjour Vincent"|mail -s "Bjr" pierre@localhost
+echo '$alexandre: echo "Bonjour Vincent"|mail -s "Bjr" pierre@localhost'
 sleep 1
+
+echo -e
+echo '$alexandre: cat /var/mail/alexandre'
+sudo -u alexandre cat /var/mail/alexandre
+
+echo -e
+echo '$pierre: cat /var/mail/pierre'
+sudo -u pierre cat /var/mail/pierre
 
 echo -e
 echo '$vincent: cat /var/mail/vincent'
