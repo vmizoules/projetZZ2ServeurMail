2dd3b217423978f763803a6c6ffc9f3faea1f533
Alex <aleeeeeexx@hotmail.fr>
2016-01-05 17:56:33 +0100

    Add fosuser bundle

--- docker/apache/apache.sh
+++ docker/apache/apache.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# wait database 
+sleep 20
+# configure symfony
+cd /var/www/html
+php bin/console doctrine:schema:update --force
+
+# run apache2
 source /etc/apache2/envvars
-rm /var/run/apache2/apache2.pid
+touch /var/log/apache2/error.log
 tail -f /var/log/apache2/error.log &
 exec apache2 -D FOREGROUND
