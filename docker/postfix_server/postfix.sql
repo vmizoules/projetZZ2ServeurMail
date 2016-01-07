75f0d5fb22e7ef62c23c98a0f5595cee05ae4d9b
Alex <aleeeeeexx@hotmail.fr>
2016-01-07 11:44:59 +0100

    Update postfix database

--- docker/postfix_server/postfix.sql
+++ docker/postfix_server/postfix.sql
@@ -2,10 +2,9 @@ USE postfix;
 CREATE TABLE `alias` (
 `address` varchar(255) NOT NULL default '',
 `goto` text NOT NULL,
-`domain` varchar(255) NOT NULL default '',
 `created` datetime NOT NULL default '0000-00-00 00:00:00',
 `modified` datetime NOT NULL default '0000-00-00 00:00:00',
-`active` tinyint(1) NOT NULL default '1',
+`enabled` tinyint(1) NOT NULL default '1',
 PRIMARY KEY  (address)
 ) ENGINE=MyISAM COMMENT='Postfix Admin - Virtual Aliases';
 
