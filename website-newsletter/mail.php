62d5be35204f1c414d70423cd919b9f069b051a1
Vincent <dark-vince15@hotmail.fr>
2016-02-12 14:29:18 +0100

    add mail command

--- website-newsletter/mail.php
+++ website-newsletter/mail.php
@@ -2,8 +2,28 @@
 
 if($_POST['email']) {
 
-	echo $_POST['email'];
+
+	$to = $_POST['email'];
+	echo $to;
+
+
 	die();
+
+	$from = "abcde@poste.isima.fr";
+
+	$subject = 'Website subscription';
+
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	$message = '<html><body>';
+	$message .= '<h1>Hello!</h1>';
+	$message .= '<p>' . $to . '</p>';
+	$message .= '</body></html>';
+
+	mail($to, $subject, $message, $headers);
 
 	header("Location: index.html");
 } else {
