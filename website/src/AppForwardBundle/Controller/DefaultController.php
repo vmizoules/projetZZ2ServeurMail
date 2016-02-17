af2e2368ce94aa65337f701ba6b093a0cd7ca4f2
vmizoules <dark-vince15@hotmail.fr>
2016-02-17 18:29:10 +0100

    use id instead of authentication (because CORS doesn't allows us to do that)

--- website/src/AppForwardBundle/Controller/DefaultController.php
+++ website/src/AppForwardBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppForwardBundle\Controller;
 
 use AppForwardBundle\Entity\Alias;
+use AppForwardBundle\Entity\User;
 use AppForwardBundle\Form\AliasType;
 use AppForwardBundle\Manager\MailAddress;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -17,50 +18,62 @@ class DefaultController extends Controller
 
         // test it with : curl --data "site=mysite&url=mysite.abc/as.php" http://127.0.0.1/web/app_dev.php/api/create
 
-        $httpCodeStatus = 401; // status: user not authenticated
+        $httpCodeStatus = 401; // status: user not authenticated (the id is not recognized)
         $data = array(
             'authenticated' => false,
             'created'       => false,
             'address'       => '',
         );
 
-        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // == user connected ==
+        // -- check if id given is good --
+        // get user_id given
+        $user_id = $request->request->get('id'); // TODO : make it better (use crypted id)
+        // get user by id in db
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->findUserBy(array('id'=>$user_id));
+
+        if($user instanceof User) {
+            // id given is good
+
             $data['authenticated'] = true;
 
-            try {
-                // create new alias
-                $newAlias = new Alias();
+            // check datas are good
+            $url_value = $request->request->get('url');
+            $site_value = $request->request->get('site');
+            if(!is_null($url_value) && !is_null($site_value)) {
+                // datas are correct
+                try {
+                    // create new alias
+                    $newAlias = new Alias();
 
-                // -- attributs -- // TODO use a manager to create ?
-                // url
-                $url_value = $request->request->get('url');
-                $site_value = $request->request->get('site'); // TODO check if empty !!!
-                $newAlias->setSite($site_value);
-                $newAlias->setUrl($url_value);
-                // user_id
-                $user_id = $this->getUserId();
-                $newAlias->setUserId($user_id);
-                // set random Address in newAlias
-                $aliasAddress = MailAddress::generateMailAddress($newAlias->getUserId(), $newAlias->getSite());
-                $newAlias->setAddress($aliasAddress);
-                // set created & modified at now
-                $now = new DateTime('now');
-                $newAlias->setCreated($now);
-                $newAlias->setModified($now);
+                    // -- attributs -- // TODO use a manager to create ?
+                    // url
+                    $newAlias->setSite($site_value);
+                    $newAlias->setUrl($url_value);
+                    // user_id
+                    $newAlias->setUserId($user_id);
+                    // set random Address in newAlias
+                    $aliasAddress = MailAddress::generateMailAddress($newAlias->getUserId(), $newAlias->getSite());
+                    $newAlias->setAddress($aliasAddress);
+                    // set created & modified at now
+                    $now = new DateTime('now');
+                    $newAlias->setCreated($now);
+                    $newAlias->setModified($now);
 
-                // create in db
-                $entityManager = $this->getEntityManager();
-                $entityManager->persist($newAlias);
-                $entityManager->flush();
+                    // create in db
+                    $entityManager = $this->getEntityManager();
+                    $entityManager->persist($newAlias);
+                    $entityManager->flush();
 
-                // set response
-                $data['address'] = $newAlias->getAddress();
-                $data['created'] = true;
-                $httpCodeStatus = 200; // status: good
-            } catch (Exception $e) {
-                $httpCodeStatus = 503; // status: service unavailable or under maintenance
+                    // set response
+                    $data['address'] = $newAlias->getAddress();
+                    $data['created'] = true;
+                    $httpCodeStatus = 200; // status: good
+                } catch (Exception $e) {
+                    $httpCodeStatus = 503; // status: service unavailable or under maintenance
+                }
             }
+
         }
 
         return new JsonResponse($data, $httpCodeStatus);
--- website/src/AppForwardBundle/Manager/MailAddress.php
+++ website/src/AppForwardBundle/Manager/MailAddress.php
@@ -5,7 +5,7 @@ namespace AppForwardBundle\Manager;
 class MailAddress
 {
     public static function generateMailAddress($username, $website) {
-        $start = md5($username.$website.microtime()."caca");
+        $start = md5($username.$website.microtime()."1234");
         $end = "@zz2postfixproject.fr";
         return $start.$end;
     }
